940c5c935486d2a73a8519cbdb22bfc0e295537d
WINDOWS-72MUN56\User <aryandh.a.w@gmail.com>
2022-05-15 13:19:50 +0700

    Unit Test kondisi - Versi Java

--- belajar-java-unit-test/src/test/java/iyai/learning/path/test/generator/ConditionTest.java
+++ belajar-java-unit-test/src/test/java/iyai/learning/path/test/generator/ConditionTest.java
@@ -1,9 +1,7 @@
 package iyai.learning.path.test.generator;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledOnOs;
-import org.junit.jupiter.api.condition.EnabledOnOs;
-import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.api.condition.*;
 
 public class ConditionTest {
     @Test
@@ -16,5 +14,29 @@ public class ConditionTest {
     @DisabledOnOs({OS.WINDOWS})
     void testDisabledOnWindows(){
         // tidak bisa running di Windows (atau OS yg ditetapkan)
+    }
+
+    @Test
+    @EnabledOnJre({JRE.JAVA_14})
+    void onlyRunJava14(){
+        // hanya bisa running di Java versi 14 (atau versi java yg dipilih)
+    }
+
+    @Test
+    @DisabledOnJre({JRE.JAVA_14})
+    void disabledRunJava14(){
+        // tidak bisa running di Java versi 14 (atau versi java yg dipilih)
+    }
+
+    @Test
+    @EnabledForJreRange(min = JRE.JAVA_11, max = JRE.JAVA_14)
+    void onlyRunJava11To14(){
+        // hanya bisa running di Java versi 11 sampai dengan 14 (atau versi java yg dipilih)
+    }
+
+    @Test
+    @DisabledForJreRange(min = JRE.JAVA_11, max = JRE.JAVA_14)
+    void disabledRunJava11To14(){
+        // tidak bisa running di Java versi 11 sampai dengan 14 (atau versi java yg dipilih)
     }
 }
