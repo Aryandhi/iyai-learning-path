451a7046bd3d9229e126d3a629631aafb0594bb3
WINDOWS-72MUN56\User <aryandh.a.w@gmail.com>
2022-05-15 10:56:11 +0700

    add custom @DisplayName

--- belajar-java-unit-test/src/test/java/iyai/learning/path/test/CalculatorTest.java
+++ belajar-java-unit-test/src/test/java/iyai/learning/path/test/CalculatorTest.java
@@ -1,11 +1,15 @@
 package iyai.learning.path.test;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Test Calculator class")
 public class CalculatorTest {
     private Calculator calculator = new Calculator();
 
+    // displayName akan tampil diterminal testing
+    @DisplayName("untuk test skenario sukses method add(Integer, Integer)")
     @Test
     public void testAddSuccess(){
         var result = calculator.add(10, 10);
@@ -18,8 +22,6 @@ public class CalculatorTest {
         assertEquals(10, result);
     }
 
-    // untuk validasi apakah pesan error pada divide() yg terdapat di class Calculator berfungsi
-    // bila throws error tidak muncul, maka test gagal walaupun hasil operasi perhitungan benar
     @Test
     public void testDivideError(){
         assertThrows(IllegalArgumentException.class, () -> {
