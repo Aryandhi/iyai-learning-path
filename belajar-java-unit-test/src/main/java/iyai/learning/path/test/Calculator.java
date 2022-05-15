a00c15bd2dccd6bff1de23e68e8bf94caa40d31a
WINDOWS-72MUN56\User <aryandh.a.w@gmail.com>
2022-05-15 10:11:11 +0700

    scenario test sukses

--- belajar-java-unit-test/src/main/java/iyai/learning/path/test/Calculator.java
+++ belajar-java-unit-test/src/main/java/iyai/learning/path/test/Calculator.java
@@ -3,6 +3,6 @@ package iyai.learning.path.test;
 public class Calculator {
 
     public Integer add(Integer first, Integer second){
-        return first * second;
+        return first + second;
     }
 }
--- belajar-java-unit-test/src/test/java/iyai/learning/path/test/CalculatorTest.java
+++ belajar-java-unit-test/src/test/java/iyai/learning/path/test/CalculatorTest.java
@@ -9,7 +9,7 @@ public class CalculatorTest {
     @Test
     public void testAddSuccess(){
         var result = calculator.add(10, 10);
-
+        // cara manual bila test tidak menggunakan assertions
         if(result != 20){
             throw new RuntimeException("Test Gagal");
         }
